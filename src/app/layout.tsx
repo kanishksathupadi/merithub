96643f0af84ac51b433e6a2f227189140593939d
kanishk sathupadi <kanishk.sathupadi@gmail.com>
2025-09-11 22:32:17 +0000

    this is what it hsows

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
