2e44bad67c1078a9b41bc5f9d32a28d015525e71
kanishk sathupadi <kanishk.sathupadi@gmail.com>
2025-09-15 23:32:20 +0000

    [Violation] Potential permissions policy violation: usb is not allowed i

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,23 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
